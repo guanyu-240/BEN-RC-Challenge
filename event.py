df5cc151e0efd1c717cb46227223da4e5a83c001
Guanyu Wang <guanyuwang.cs@gmail.com>
2016-11-18 17:28:56 -0500

    Create event.py

---
+++ event.py
@@ -0,0 +1,16 @@
+import ConfigParser
+from data import EventData
+
+def convert_date_str(s):
+  return datetime.strptime(s, '%Y-%m-%d').date()
+
+class Event:
+  def __init__(self, cfg_file, event_name):
+    cfg = ConfigParser.RawConfigParser()
+    cfg.read(cfg_file)
+    start_date = convert_date_str(cfg.get(event_name, 'start_date'))
+    end_date = convert_date_str(cfg.get(event_name, 'end_date'))
+    data_file = cfg.get(event_name, 'data_file')
+    self.__data = EventData(data_file, start_date, end_date)
+    
+  
