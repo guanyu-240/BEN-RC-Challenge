707804aff3420b80cb1dcfa7a82e2f506c805405
Predrag Wang <guanyuwang.cs@gmail.com>
2016-11-20 18:31:29 -0500

    Added update process

--- webhost.py
+++ webhost.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python
 from flask import Flask, request, render_template
-from stravalib.strava import Strava
+from stravalib.strava import Strava, process_activity
 from event import get_event_data 
+from multiprocessing import Process
+import ConfigParser
+import time
 
 app = Flask(__name__)
 
 data = get_event_data('event.cfg', 'running_streak_1')
-strava_obj = Strava('d575147b36c3611231ca55f807307f16d06c8eef')
+cfg = ConfigParser.RawConfigParser()
+cfg.read('website.cfg')
+access_token = cfg.get('website', 'strava_access_token')
+club_id = cfg.getint('website', 'club_id')
+strava_obj = Strava(access_token)
 
 @app.route("/event_register")
 def event_register():
@@ -19,6 +26,25 @@ def event_stats():
     week_idx = int(request.form.get('week_idx'))
   weekly_data = data.get_weekly_data(week_idx)
   return render_template('event_stats.html', weekly_data=weekly_data)
+
+def update_data():
+  activities = strava_obj.getClubActivitiesCurWeek(club_id, time_zone='US/Eastern')
+  for a in activities:
+    a = process_activity(a)
+    data.add_activity(a)
+  data.update_weekly_scores(data.get_current_week_idx())
+  data.save_data()
+
+def update_data_process():
+  while True:
+    update_data()
+    print "Updated"
+    time.sleep(500)
+
+p = Process(target=update_data_process)
+p.start()
+p.join()
+
 
 if __name__ == "__main__":
   app.run()
