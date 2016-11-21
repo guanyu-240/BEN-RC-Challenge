c3c1c06ad2bc645293e7a1b89c841f3d10daee2e
Predrag Wang <guanyuwang.cs@gmail.com>
2016-11-21 15:38:54 -0500

    reorganize event config

--- event.py
+++ event.py
@@ -5,11 +5,17 @@ from datetime import datetime
 def convert_date_str(s):
   return datetime.strptime(s, '%Y-%m-%d').date()
 
-def get_event_data(cfg_file, event_name):
-  cfg = ConfigParser.RawConfigParser()
-  cfg.read(cfg_file)
-  start_date = convert_date_str(cfg.get(event_name, 'start_date'))
-  end_date = convert_date_str(cfg.get(event_name, 'end_date'))
-  data_file = cfg.get(event_name, 'data_file')
-  return EventData(data_file, start_date, end_date)
+class EventConfig:
+  def __init__(self, cfg_file):
+    self.cfg = ConfigParser.RawConfigParser()
+    self.cfg.read(cfg_file)
+
+  def get_events(self):
+    return self.cfg.sections() 
+
+  def get_event_data(self, event_name):
+    start_date = convert_date_str(self.cfg.get(event_name, 'start_date'))
+    end_date = convert_date_str(self.cfg.get(event_name, 'end_date'))
+    data_file = self.cfg.get(event_name, 'data_file')
+    return EventData(data_file, start_date, end_date)
   
